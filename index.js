af013cdaf9a537538d8dad691634fe0fdff75720
Nicolas Cardenas <nico.cardenas260494@gmail.com>
2023-08-14 20:13:37 -0300

    se le agrega sweet y storage

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ console.log(inputUbi)
 
 
 
-// Crear un botón de inicio de sesión
+// Crear un botón
 const botonLogin = document.createElement('button');
 botonLogin.id = "botonDeEnviar"
 botonLogin.type = 'submit';
@@ -63,9 +63,11 @@ formulario.appendChild(botonLogin);
 // Agregar el formulario al contenedor
 formularioContainer.appendChild(formulario);
 
+
 // Crear el evento para el envío del formulario
 formulario.addEventListener('submit', function (event) {
     event.preventDefault();
+
 
 
 
@@ -84,31 +86,49 @@ formulario.addEventListener('submit', function (event) {
         numeroDeCelular.trim() === '' ||
         ubicacionCliente.trim() === ''
     ) {
-        alert('Por favor, complete todos los campos antes de enviar el formulario.');
+        Swal.fire(
+            'Por favor complete todos los casilleros',
+        )
         return;
     }
     //validar nombre de usuario
     const regexNombreUsuario = /^[a-zA-Z]{1,19}$/;
     if (!regexNombreUsuario.test(nombreUsuario)) {
-        alert('El nombre de usuario debe contener solo letras, menos de 20 caracteres y no dejar espacios.');
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'El nombre de usuario debe contener solo letras, menos de 20 caracteres y no dejar espacios.!',
+        });
         return;
     }
 
     //Validar cantidad de dinero
     if (dineroSolicitado <= 99999 || dineroSolicitado > 100000000) {
-        alert('La cantidad de dinero a prestar debe ser mayor a 100.000 y como máximo 100,000,000.');
-        return; // Detener el proceso si la cantidad de dinero no cumple con la validación
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: "La cantidad de dinero a prestar debe ser mayor a 100.000 y como máximo 100,000,000.!",
+        });
+        return;
     }
 
     //validar cantidad de cuotas
     if (cuotasSolicitadas > 60 || cuotasSolicitadas < 3) {
-        alert('El número máximo de cuotas es 60 y el minimo de 3. Por favor, ingrese un valor válido.');
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: "El número máximo de cuotas es 60 y el minimo de 3. Por favor, ingrese un valor válido.!",
+        });
         return;
     }
 
     if (numeroDeCelular.length > 12) {
-        alert('Numero ingresado no valido');
-        return; // Detener el proceso si el número de celular no tiene 12 dígitos
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: "El número ingresado no es valido!",
+        });
+        return;
     }
 
 
@@ -116,9 +136,17 @@ formulario.addEventListener('submit', function (event) {
     const provinciasDeArgentina = ["buenos aires", "catamarca", "chaco", "chubut", "cordoba", "Corrientes", "entre rios", "formosa", "jujuy", "la pampa", "la rioja", "mendoza", "misiones", "neuquen", "rio negro", "salta", "san juan", "san luis", "santa cruz", "santa fe", "santiago del estero", "tierra del fuego", "tucuman"];
 
     if (!provinciasDeArgentina.includes(ubicacionCliente)) {
-        alert('La ubicación ingresada no es válida. Por favor, seleccione una provincia válida.');
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: "La ubicación ingresada no es válida. Por favor, seleccione una provincia válida.!",
+        });
         return;
     }
+
+
+    // Deshabilitar el botón después de enviar el formulario
+    botonLogin.disabled = true;
 
     //Se asigna esos valores a un cliente (objeto)
     const datosCliente = {
@@ -127,8 +155,48 @@ formulario.addEventListener('submit', function (event) {
         cuotasSolicitadas: cuotasSolicitadas,
         numeroDeCelular: numeroDeCelular,
         ubicacionCliente: ubicacionCliente,
-        dineroAPagar: dineroSolicitado / cuotasSolicitadas,
+        dineroAPagar: dineroSolicitado * 1.75,
 
     };
-    console.log(datosCliente)
+
+    // Se envia el cliente al storage y se crea un saludo
+
+    localStorage.setItem("nuevoCliente", JSON.stringify(datosCliente))
+    const nCliente = localStorage.getItem("nuevoCliente")
+    const nClienteParseado = JSON.parse(nCliente)
+
+    const saludoCliente = document.createElement("h4")
+    saludoCliente.innerHTML = ("Bienvenido " + nClienteParseado.nombreUsuario + " su prestamo es de " + "$" + nClienteParseado.dineroSolicitado + " y obtiene un interés del 75% por lo cual va a tener que pagar " + "$" + nClienteParseado.dineroAPagar + " en las siguientes " + nClienteParseado.cuotasSolicitadas + " cuotas, a continuacion se detalla el valor de cada una :")
+
+    formularioContainer.appendChild(saludoCliente)
+
+
+    //tabla de cuotas
+    const tablaCuotas = document.createElement('table');
+    tablaCuotas.classList.add('table', 'table-striped');
+    tablaCuotas.innerHTML = `
+    <thead class="thead-dark">
+        <tr>
+            <th>Número de Cuota</th>
+            <th>Valor de Cuota</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+`;
+    formularioContainer.appendChild(tablaCuotas);
+
+    const cuerpoTabla = tablaCuotas.querySelector('tbody');
+
+    // Calcular y mostrar las cuotas
+    for (let i = 1; i <= cuotasSolicitadas; i++) {
+        const valorCuota = (dineroSolicitado / cuotasSolicitadas) * 1.75;
+        const fila = `
+        <tr>
+            <td>${i}</td>
+            <td>${valorCuota.toFixed(2)}</td>
+        </tr>
+    `;
+        cuerpoTabla.innerHTML += fila;
+    }
 });
